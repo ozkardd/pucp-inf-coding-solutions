8955bb21c7c9e40f51bd5280d02335edf3dde7ec
Oscar Dueñas <70769251+ozkardd@users.noreply.github.com>
2023-05-11 02:45:45 -0500

    Add files via upload

---
+++ INF246 - Bases de Datos/Laboratorios/L2/2018-2/datapersona.sql
@@ -0,0 +1,61 @@
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 1, 'Tovar', 'Luisa', 'F', 2500, 3 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 2, 'Perez', 'Marta', 'F', 3000, 1 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 3, 'Lopez', 'Daniel', 'M', 4200, 2 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 4, 'Jimenez',	'Karla', 'F', 1300, 5 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 5, 'Abarca', 'Pedro', 'M', 1700, 4 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 6, 'Zapata', 'Jorge', 'M', 2400, 1 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 7, 'Talavera', 'Hector', 'M',  2100, 1 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 8, 'Fernandez', 'Luisa', 'F', 5000, 3 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 9, 'Lopez', 'Elsa', 'F', 5400, 2 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 10, 'Lazarte', 'Margarita', 'F', 2200, 3 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 11, 'Fernandez', 'Olenka', 'F', 1800, 2 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 12, 'Lovaton', 'Ronald', 'M', 3100, 1 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 13, 'Davila', 'Pedro', 'M', 2100, 4 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 14, 'Carrasco', 'Rosa', 'F', 2800, 3 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 15, 'Lazo', 'Juan', 'M', 1800, 4 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 16, 'Segovia', 'Victoria', 'F', 3000, 1 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 17, 'Vela', 'Jorge', 'M', 1700, 2 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 18, 'Polar', 'Guillermo', 'F', 4200, 1 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 19, 'Linares', 'Juan', 'M', 2000, 3 );
+
+INSERT INTO PERSONA (IdPersona,Apellido,Nombre,Sexo,Sueldo,IdDistrito)
+VALUES ( 20, 'Torres', 'Rosa', 'F', 3200, 4 );
+
+COMMIT;
